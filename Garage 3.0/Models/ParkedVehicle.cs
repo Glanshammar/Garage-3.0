c9fb9c6662f9301e50131490e43d16a2f56fc670
Karin Wegner <karinwegner00@gmail.com>
2024-11-18 14:51:58 +0100

    Added base types without connections

---
+++ Garage 3.0/Models/ParkedVehicle.cs
@@ -0,0 +1,12 @@
+﻿namespace Garage_3._0.Models
+{
+    public class ParkedVehicle
+    {
+        public int Id { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public VehicleType VehicleType { get; set; }
+    }
+}
