8b47221404f44fe6f0e6bddf24f0f2c3b4c685e1
Karin Wegner <karinwegner00@gmail.com>
2024-11-18 15:26:48 +0100

    Removed Id from VehicleType Set Name to primary key

--- Garage 3.0/Data/ApplicationDbContext.cs
+++ Garage 3.0/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Garage_3._0.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Garage_3._0.Data
@@ -8,6 +9,16 @@ namespace Garage_3._0.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<ParkedVehicle> ParkedVehicles { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<VehicleType>()
+                .HasKey(t => new { t.Name });
+            
         }
     }
 }
--- Garage 3.0/Models/VehicleType.cs
+++ Garage 3.0/Models/VehicleType.cs
@@ -2,7 +2,8 @@
 {
     public class VehicleType
     {
-        public int Id { get; set; }
         public string Name { get; set; }
+
+        public ICollection<ParkedVehicle> Vehicles { get; set; }
     }
 }
