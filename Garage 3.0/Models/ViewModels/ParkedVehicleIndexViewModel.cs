2440d2beb8e7e508297fbc389b4a0c2de6106da0
Karin Wegner <karinwegner00@gmail.com>
2024-11-19 14:16:18 +0100

    Added VehicleIndexViewModel

---
+++ Garage 3.0/Models/ViewModels/ParkedVehicleIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Garage_3._0.Models.ViewModels
+{
+    public class ParkedVehicleIndexViewModel
+    {
+        public int Id { get; set; }
+        public string RegistrationNumber { get; set; }
+
+        //Foreign Keys
+        public VehicleType VehicleType { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public ParkingSpot ParkingSpot { get; set; }
+    }
+}
