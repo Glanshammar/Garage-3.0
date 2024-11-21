1f1e7e32d7a5b9664aaf3a3e99ae521796dbcf47
Karin Wegner <karinwegner00@gmail.com>
2024-11-21 09:10:37 +0100

    Made CreateParkedVehicleViewModel

---
+++ Garage 3.0/Models/ViewModels/CreateParkedVehicleViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Garage_3._0.Models.ViewModels
+{
+    public class CreateParkedVehicleViewModel
+    {
+        public int Id { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public string Color { get; set; }
+
+        //Foreign Keys
+        public VehicleType VehicleType { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public IEnumerable<VehicleType> VehicleTypes { get; set; }
+
+       // public ParkingSpot ParkingSpot { get; set; }
+    }
+}
