7bb27e8496bed2ba87ce10ad3a5660850650cc35
Andreas Johansson <glanshammar70@gmail.com>
2024-11-21 11:55:07 +0100

    Added VehicleInput view model

---
+++ Garage 3.0/Models/ViewModels/VehicleInputViewModel.cs
@@ -0,0 +1,10 @@
+namespace Garage_3._0.Models.ViewModels;
+
+public class VehicleInputViewModel
+{
+    public string RegistrationNumber { get; set; }
+    public string Model { get; set; }
+    public string Brand { get; set; }
+    public string Color { get; set; }
+    public int ParkingSpotId { get; set; }
+}
