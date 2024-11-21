5ea35b4f5a0b66248a1ebab66ffe2bf4a0d2c776
Andreas Johansson <glanshammar70@gmail.com>
2024-11-21 11:41:11 +0100

    Added one-to-one relationship

--- Garage 3.0/Data/ApplicationDbContext.cs
+++ Garage 3.0/Data/ApplicationDbContext.cs
@@ -15,7 +15,6 @@ namespace Garage_3._0.Data
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<ParkingSpot> ParkingSpots { get; set; }
 
-
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
@@ -23,29 +22,29 @@ namespace Garage_3._0.Data
             builder.Entity<VehicleType>()
                 .HasKey(t => new { t.Name });
 
-           builder.Entity<ApplicationUser>()
-          .HasIndex(u => u.PersonalNumber)
-          .IsUnique();
+            builder.Entity<ApplicationUser>()
+                .HasIndex(u => u.PersonalNumber)
+                .IsUnique();
 
+            // Configuring one-to-one relationship between ParkingSpot and ParkedVehicle
+            builder.Entity<ParkingSpot>()
+                .HasOne(p => p.ParkedVehicle) // A parking spot has one parked vehicle
+                .WithOne(v => v.ParkingSpot)  // A parked vehicle belongs to one parking spot
+                .HasForeignKey<ParkedVehicle>(v => v.ParkingSpotId); // Foreign key in the parked vehicle
 
-            // Testdata för ParkingSpots
+            // Seed data for ParkingSpots
             builder.Entity<ParkingSpot>().HasData(
                 new ParkingSpot { Id = 1, SpotNumber = "A1", Size = "Small", Location = "North", IsOccupied = false },
                 new ParkingSpot { Id = 2, SpotNumber = "B2", Size = "Medium", Location = "South", IsOccupied = true },
                 new ParkingSpot { Id = 3, SpotNumber = "C3", Size = "Large", Location = "East", IsOccupied = false }
             );
 
-            // Testdata för VehicleType
+            // Seed data for VehicleType
             builder.Entity<VehicleType>().HasData(
                 new VehicleType { Name = "Car" },
                 new VehicleType { Name = "Motorcycle" },
                 new VehicleType { Name = "Bus" }
             );
-
-
-
         }
-
-      
     }
 }
